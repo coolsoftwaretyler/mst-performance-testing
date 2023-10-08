2d7c09c1ae0de91c9e453a02323ada3e4aaa401d
Tyler Williams <tyler@coolsoftware.dev>
2023-10-08 17:06:13 -0400

    feat: adds better memory measurement

--- runner.js
+++ runner.js
@@ -3,28 +3,83 @@ import Benchmark from "benchmark"; // This is technically a no-op in the web bun
 
 var suite = new Benchmark.Suite();
 
+/**
+ * getStartMemory is an isomorphic way to get the memory used, in bytes,
+ * either from the browser or from Node.
+
+ */
+const getStartMemory = () => {
+  if (typeof performance.memory !== "undefined") {
+    return performance.memory.usedJSHeapSize;
+  } else {
+    const used = process.memoryUsage();
+    return used.heapUsed;
+  }
+};
+
+/**
+ * getEndMemory is an isomorphic way to get the memory used, in bytes,
+ * either from the browser or from Node.
+ */
+const getEndMemory = () => {
+  if (typeof performance.memory !== "undefined") {
+    return performance.memory.usedJSHeapSize;
+  } else {
+    const used = process.memoryUsage();
+    return used.heapUsed;
+  }
+};
+
+/**
+ * trackMaxMemory maintains a map of memory usage for each scenario.
+ *
+ * It uses keys that correspond to the title of each scenario, and the
+ * value is the maximum memory used for that scenario.
+ */
+const memoryUsage = {};
+const trackMaxMemory = (title, memoryUsed) => {
+  if (!memoryUsage[title]) {
+    memoryUsage[title] = memoryUsed;
+  } else {
+    memoryUsage[title] = Math.max(memoryUsage[title], memoryUsed);
+  }
+};
+
 suite.on("complete", function () {
-  const headers = ["scenario", "ops_per_sec", "margin_of_error", "runs"];
+  const headers = [
+    "scenario",
+    "ops_per_sec",
+    "margin_of_error",
+    "runs",
+    "max_memory_used_kb",
+  ];
   const results = suite.filter("successful").map((benchmark) => {
     return {
       scenario: benchmark.name,
       opsSec: benchmark.hz,
       plusMinus: benchmark.stats.rme,
       runs: benchmark.stats.sample.length,
+      maxMemory: memoryUsage[benchmark.name],
     };
   });
 
   console.log(headers.join(","));
   results.forEach((result) =>
     console.log(
-      `${result.scenario},${result.opsSec},${result.plusMinus},${result.runs}`
+      `${result.scenario},${result.opsSec},${result.plusMinus},${result.runs},${result.maxMemory}`
     )
   );
 });
 
 for (const scenario in Scenarios) {
   const { title, run } = Scenarios[scenario];
-  suite.add(title, run);
+  suite.add(title, () => {
+    const startMemory = getStartMemory();
+    run();
+    const endMemory = getEndMemory();
+    const memoryUsed = endMemory - startMemory;
+    trackMaxMemory(title, memoryUsed);
+  });
 }
 
 suite.run();
