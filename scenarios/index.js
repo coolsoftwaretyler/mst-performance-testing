2d4378d3893ce7772953ba6e5ae0f052df41ca85
Tyler Williams <tyler@coolsoftware.dev>
2023-10-21 22:14:24 -0600

    feat: add scenarios for actions/views on creation

--- scenarios/index.js
+++ scenarios/index.js
@@ -386,3 +386,155 @@ export const scenario36 = {
     zodTypeCheckFailure();
   },
 };
+
+export const scenario37 = {
+  title: "Create a model and add actions to it",
+  run: () => {
+    const Model = types
+      .model({
+        string: types.string,
+        number: types.number,
+        integer: types.integer,
+        float: types.float,
+        boolean: types.boolean,
+        date: types.Date,
+      })
+      .actions((self) => ({
+        setString: (string) => {
+          self.string = string;
+        },
+        setNumber: (number) => {
+          self.number = number;
+        },
+        setInteger: (integer) => {
+          self.integer = integer;
+        },
+        setFloat: (float) => {
+          self.float = float;
+        },
+        setBoolean: (boolean) => {
+          self.boolean = boolean;
+        },
+        setDate: (date) => {
+          self.date = date;
+        },
+      }));
+
+    Model.create({
+      string: "string",
+      number: 1,
+      integer: 1,
+      float: 1.1,
+      boolean: true,
+      date: new Date(),
+    }).setString("new string");
+  },
+};
+
+export const scenario38 = {
+  title: "Create a model and add views to it",
+  run: () => {
+    const Model = types
+      .model({
+        string: types.string,
+        number: types.number,
+        integer: types.integer,
+        float: types.float,
+        boolean: types.boolean,
+        date: types.Date,
+      })
+      .views((self) => ({
+        getString: () => {
+          return self.string;
+        },
+        getNumber: () => {
+          return self.number;
+        },
+        getInteger: () => {
+          return self.integer;
+        },
+        getFloat: () => {
+          return self.float;
+        },
+        getBoolean: () => {
+          return self.boolean;
+        },
+        getDate: () => {
+          return self.date;
+        },
+      }));
+
+    Model.create({
+      string: "string",
+      number: 1,
+      integer: 1,
+      float: 1.1,
+      boolean: true,
+      date: new Date(),
+    }).getString();
+  },
+};
+
+export const scenario39 = {
+  title: "Create a model and add both actions and views to it",
+  run: () => {
+    const Model = types
+      .model({
+        string: types.string,
+        number: types.number,
+        integer: types.integer,
+        float: types.float,
+        boolean: types.boolean,
+        date: types.Date,
+      })
+      .actions((self) => ({
+        setString: (string) => {
+          self.string = string;
+        },
+        setNumber: (number) => {
+          self.number = number;
+        },
+        setInteger: (integer) => {
+          self.integer = integer;
+        },
+        setFloat: (float) => {
+          self.float = float;
+        },
+        setBoolean: (boolean) => {
+          self.boolean = boolean;
+        },
+        setDate: (date) => {
+          self.date = date;
+        },
+      }))
+      .views((self) => ({
+        getString: () => {
+          return self.string;
+        },
+        getNumber: () => {
+          return self.number;
+        },
+        getInteger: () => {
+          return self.integer;
+        },
+        getFloat: () => {
+          return self.float;
+        },
+        getBoolean: () => {
+          return self.boolean;
+        },
+        getDate: () => {
+          return self.date;
+        },
+      }));
+
+    Model.create({
+      string: "string",
+      number: 1,
+      integer: 1,
+      float: 1.1,
+      boolean: true,
+      date: new Date(),
+    }).getString();
+  },
+};
